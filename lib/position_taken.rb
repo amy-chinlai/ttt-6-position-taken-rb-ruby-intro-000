9cf0101aee09bc6f76ee63b19ac8648fefca7738
Amy Chin-Lai <amy.chinlai@gmail.com>
2019-11-13 17:53:06 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,7 @@ def position_taken?(board, index)
   if board[index] == " " || board[index] != "" || board[index] != nil
     false
   elsif
-    board[index] == "X" || board[index] == "0"
+    board[index] == "X" || board[index] == "O"
     true
   else
     nil
